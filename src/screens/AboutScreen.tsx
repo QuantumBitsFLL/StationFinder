84ad5dbce997e63893b75efa7ba30a0e81bbe49f
Alex Stan <stan.alexandru@ichb.ro>
2023-03-21 21:23:18 +0200

    feat: add AboutScreen
    Signed-off-by: Alex Stan <stan.alexandru@ichb.ro>

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+<<<<<<< HEAD
 import { View, Text, StyleSheet } from 'react-native';
+=======
+import { View, Text } from 'react-native';
+>>>>>>> f7d053e (feat: add AboutScreen)
 
 const AboutScreen = ( { navigation } ) => {
     return (
         <View>
+<<<<<<< HEAD
             <Text style = { styles.title }>About screen</Text>
+=======
+            <Text>Hello, World!</Text>
+>>>>>>> f7d053e (feat: add AboutScreen)
         </View>
     )
 }
 
+<<<<<<< HEAD
 const styles = StyleSheet.create({
     title: {
         fontSize: 32,
@@ -18,4 +27,6 @@ const styles = StyleSheet.create({
     }
 })
 
+=======
+>>>>>>> f7d053e (feat: add AboutScreen)
 export default AboutScreen;
