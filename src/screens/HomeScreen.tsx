aac9010f0566f412fad37ba79953559b56800e4d
Alex Stan <stan.alexandru@ichb.ro>
2023-03-29 15:31:03 +0300

    feat: refactor code
    Signed-off-by: Alex Stan <stan.alexandru@ichb.ro>

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,12 +4,13 @@ import { Pressable, View, Text, StyleSheet } from 'react-native';
 import TextButton from '../components/TextButton';
 import styles from '../styles';
 import { buttonStyle } from '../styles';
+
 const HomeScreen = ({ navigation }) => {
     return (
         <View style = { styles.screen }>
             <Text style = { styles.title }>Station Finder</Text>
-            <TextButton onPress = { navigation.navigate( 'HomeMap' ) }>Map</TextButton>
-            <TextButton onPress = { navigation.navigate( 'AboutScreen' ) }>About</TextButton>
+            <TextButton onPress = { () => navigation.navigate( 'HomeMap' ) }>Map</TextButton>
+            <TextButton onPress = { () => navigation.navigate( 'AboutScreen' ) }>About</TextButton>
         </View>
     )
 };
